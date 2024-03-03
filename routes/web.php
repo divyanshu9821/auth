7dfc2b7c5e2a2f84dd4673e85a29e145c0bc68ab
divyanshu9821 <singhaldivyanshu2001@gmail.com>
2024-03-03 23:25:27 +0530

    login_func

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth_Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/sign-up',[Auth_Controller::class,'sign_up']);
+Route::get('/login',[Auth_Controller::class,'login']);
